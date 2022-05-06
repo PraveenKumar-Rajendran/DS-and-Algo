085d52888dd384f8c2f8b402358ff8a8e64f5790
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-06 14:02:09 +0900

    Time: 182 ms (38.43%), Space: 18.8 MB (39.82%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -11,7 +11,7 @@ class Solution:
             
             if stack[-1][1] == k:
                 stack.pop() # pop it if it reaches the k duplicates
-        res = "" # Initialize string
+        res = str() # Initialize string
         for char, count in stack:
             res += (char * count)
         
