a5543dbae7111cb3deaecc4e67f343e6a14aaba6
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-13 21:56:03 +0900

    Time: 54 ms (84.52%), Space: 14.2 MB (53.90%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -2,10 +2,10 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
         def Char_Counter(word):
-            whatever = defaultdict(int)
+            count_dict = defaultdict(int)
             for i in word:
-                whatever[i] += 1
-            return whatever
+                count_dict[i] += 1
+            return count_dict
         
         ran = Char_Counter(ransomNote)
         mag = Char_Counter(magazine)
