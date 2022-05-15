cd33247be964f41818fcd2d4a78357020e6319c3
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-15 20:08:19 +0900

    Time: 337 ms (31.40%), Space: 17.9 MB (28.19%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        
+        q = deque() # create queue
+        
+        q.append((root, 0)) # push initial root and its depth
+        
+        deep_sum = 0 # initialize output sum
+        max_depth = 0 # initialize maximum depth of a tree
+        
+        while q: #loop until the end of the tree(BFS)
+            
+            current, depth = q.popleft() # pop node and its depth
+            
+            if depth>max_depth: # check if the depth is growing, then zero out the sum and reset new max depth
+                deep_sum = 0
+                max_depth = depth
+                
+            if depth == max_depth: # sum the values in the max depth
+                deep_sum += current.val
+            
+            # if current node has left or right node append it, and increase the depth by 1
+            if current.left: q.append((current.left, depth+1)) 
+            if current.right: q.append((current.right, depth+1)) 
+             
+        return deep_sum
+            
+        
