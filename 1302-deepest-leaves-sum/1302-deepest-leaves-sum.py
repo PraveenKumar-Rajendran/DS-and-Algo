6e1c60cc44e3a04eec3b730d32c88155e42ea327
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-15 20:24:32 +0900

    Time: 267 ms (60.95%), Space: 17.8 MB (66.46%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -7,28 +7,31 @@
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         
-        q = deque() # create queue
+#         q = deque() # create queue
         
-        q.append((root, 0)) # push initial root and its depth
+#         q.append((root, 0)) # push initial root and its depth
         
-        deep_sum = 0 # initialize output sum
-        max_depth = 0 # initialize maximum depth of a tree
+#         deep_sum = 0 # initialize output sum
+#         max_depth = 0 # initialize maximum depth of a tree
         
-        while q: #loop until the end of the tree(BFS)
+#         while q: #loop until the end of the tree(BFS)
             
-            current, depth = q.popleft() # pop node and its depth
+#             current, depth = q.popleft() # pop node and its depth
             
-            if depth>max_depth: # check if the depth is growing, then zero out the sum and reset new max depth
-                deep_sum = 0
-                max_depth = depth
+#             if depth>max_depth: # check if the depth is growing, then zero out the sum and reset new max depth
+#                 deep_sum = 0
+#                 max_depth = depth
                 
-            if depth == max_depth: # sum the values in the max depth
-                deep_sum += current.val
+#             if depth == max_depth: # sum the values in the max depth
+#                 deep_sum += current.val
             
-            # if current node has left or right node append it, and increase the depth by 1
-            if current.left: q.append((current.left, depth+1)) 
-            if current.right: q.append((current.right, depth+1)) 
+#             # if current node has left or right node append it, and increase the depth by 1
+#             if current.left: q.append((current.left, depth+1)) 
+#             if current.right: q.append((current.right, depth+1)) 
              
-        return deep_sum
-            
-        
+        q = [root]
+        while q:
+            pre, q = q, [child for p in q for child in [p.left, p.right] if child]
+        return sum(node.val for node in pre)
+    
+# https://leetcode.com/problems/deepest-leaves-sum/discuss/463239/JavaC%2B%2BPython-Level-Traversal
