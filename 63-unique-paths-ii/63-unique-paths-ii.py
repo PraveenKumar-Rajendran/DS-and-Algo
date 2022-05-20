00f264eba250954fe709d63e15053a374dc41715
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-20 12:05:20 +0900

    Time: 71 ms (26.52%), Space: 14 MB (42.47%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        
+        m_rows, n_cols = len(obstacleGrid), len(obstacleGrid[0])
+        
+        #Check the initial square
+        if (obstacleGrid[0][0] == 1):
+            return 0
+         
+        # if passed the above check set possible path 
+        obstacleGrid[0][0] = 1
+        
+        # Filling the values for the first column # update possible paths
+        for i in range(1,m_rows):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+
+        # Filling the values for the first row # update possible paths        
+        for j in range(1, n_cols):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+        
+        for i in range(1,m_rows): # Start searching from (1,1) square and sum from top and left squares
+            for j in range(1,n_cols):
+                
+                if (obstacleGrid[i][j] == 0):
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                else:
+                    obstacleGrid[i][j] = 0
+                    
+        return obstacleGrid[m_rows-1][n_cols-1] #Return possible unique paths to the destination
