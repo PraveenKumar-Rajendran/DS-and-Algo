3b3e1c06b24ba66cce4c69b1397cc67d258c67f6
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-28 13:43:58 +0900

    Time: 216 ms (37.92%), Space: 15.2 MB (34.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,15 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        # Sum approach
+        # # Sum approach
         # res = len(nums)
         # for i in range(len(nums)):
         #     res +=  (i- nums[i])
         # return res
         
-        #XOR Approach
-        res = len(nums)
-        for i in range(len(nums)):
-            res ^= (i^nums[i])
-        return res
+        # #XOR Approach
+        # res = len(nums)
+        # for i in range(len(nums)):
+        #     res ^= (i^nums[i])
+        # return res
+        
+        #Gauss
+        return int((len(nums)*(len(nums) + 1) / 2) - sum(nums))
             
