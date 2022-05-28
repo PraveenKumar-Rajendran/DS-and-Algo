2844bf231d80e0defd3787efeddece9a794d26fe
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-28 13:44:19 +0900

    Time: 193 ms (48.19%), Space: 15.1 MB (77.27%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -7,12 +7,12 @@ class Solution:
         #     res +=  (i- nums[i])
         # return res
         
-        # #XOR Approach
-        # res = len(nums)
-        # for i in range(len(nums)):
-        #     res ^= (i^nums[i])
-        # return res
+        #XOR Approach
+        res = len(nums)
+        for i in range(len(nums)):
+            res ^= (i^nums[i])
+        return res
         
-        #Gauss
-        return int((len(nums)*(len(nums) + 1) / 2) - sum(nums))
+        # #Gauss
+        # return int((len(nums)*(len(nums) + 1) / 2) - sum(nums))
             
