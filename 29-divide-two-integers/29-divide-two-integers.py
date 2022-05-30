990f436e09f28a2871ef203fa380c68ebf7f5709
Praveen Kumar Rajendran <53290706+PraveenKumar-Rajendran@users.noreply.github.com>
2022-05-30 19:32:07 +0900

    Time: 45 ms (54.56%), Space: 13.9 MB (76.13%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,24 +1,37 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         
-        #XOR
-        sign = -1 if (dividend < 0) ^ (divisor < 0 ) else 1
+#         #XOR
+#         sign = -1 if (dividend < 0) ^ (divisor < 0 ) else 1
         
-        dividend = abs(dividend); divisor = abs(divisor)
+#         dividend = abs(dividend); divisor = abs(divisor)
         
-        quotient = 0
-        while(dividend >= divisor):
-            tmp = divisor
-            mul = 1
-            while(dividend >= tmp):
-                dividend -= tmp
-                quotient += mul
-                mul += mul
-                tmp += tmp
+#         quotient = 0
+#         while(dividend >= divisor):
+#             tmp = divisor
+#             mul = 1
+#             while(dividend >= tmp):
+#                 dividend -= tmp
+#                 quotient += mul
+#                 mul += mul
+#                 tmp += tmp
             
-        if sign == -1:
-            quotient = - quotient
+#         if sign == -1:
+#             quotient = - quotient
         
-        #Bound check
-        return min(2147483647,max(-2147483648,quotient))
+#         #Bound check
+#         return min(2147483647,max(-2147483648,quotient))
+#https://leetcode.com/problems/divide-two-integers/discuss/2089426/100-or-0MS-or-3-WAYS-or-EXPLAINED-or-MEME
+
+        if (dividend == -2147483648 and divisor == -1): return 2147483647
+        
+        a, b, res = abs(dividend), abs(divisor), 0
+        
+        for x in range(32)[::-1]:
+            
+            if (a >> x) - b >= 0:
+                res += 1 << x
+                a -= b << x
+                
+        return res if (dividend > 0) == (divisor > 0) else -res
         
